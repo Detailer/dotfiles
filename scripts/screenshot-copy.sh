6461ad4aa28093c5cb6c35a277506c7c433b8fa9
Detailer <karan.arunverma@gmail.com>
2017-11-24 20:26:44 +0530

    screenshot copy to clipboard polybar i3 config minor adds

---
+++ scripts/screenshot-copy.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#gnome-screenshot -c
+scrot -e 'mv $f ~/Pictures/Screenshots'
+LOCATION=$(ls ~/Pictures/Screenshots/ -Art | tail -n 1)
+#echo $LOCATION
+sleep 0.25s
+xclip -selection clipboard -t image/png -i "/home/hanuman/Pictures/Screenshots/$LOCATION" 
+notify-send "Screenshot Copied to Clipboard."
