0ac6821b68abf67efb8ad78b345e6bfa393631bf
Detailer <karan.arunverma@gmail.com>
2017-11-25 13:13:57 +0530

    new screenshot, modified screenshot script to save to clipboard

--- scripts/screenshot.sh
+++ scripts/screenshot.sh
@@ -1,3 +1,7 @@
+#!/bin/sh
 scrot -e 'mv $f ~/Pictures/Screenshots'
+LOCATION=$(ls ~/Pictures/Screenshots/ -Art | tail -n 1)
+#echo $LOCATION
 sleep 0.25s
+xclip -selection clipboard -t image/png -i "/home/hanuman/Pictures/Screenshots/$LOCATION" 
 notify-send "Screenshot Captured."
